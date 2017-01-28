9271d7ab3d61cd67e0315003c69f73e05a5997c9
Jonathan Adalin <JonathanAdalin@gmail.com>
2017-01-28 18:39:24 -0500

    Added user authentication

--- mchacks17/mchacks17/ViewController.swift
+++ mchacks17/mchacks17/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("the console is here!")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func CreateAccount(_ sender: Any) {
+        FIRAuth.auth()?.createUser(withEmail: Username.text!, password: Password.text!, completion: {
+            user, error in
+            
+            if error != nil { // if there is an error
+                // if account has already been created
+                self.login()
+            }
+            else{
+                print("User Created")
+                self.login()
+            }
+        })
+    }
+    
+    func login(){
+        FIRAuth.auth()?.signIn(withEmail: Username.text!, password: Password.text!, completion: {
+            user, error in
+            if error != nil {
+                print("Incorrect password/username")
+            } else {
+                print("You logged in!")
+            }
+        })
+    }
 
 }
 
