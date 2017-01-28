cd4f3835139f79c3075df97b692da7d33a69b4bd
Jonathan Adalin <JonathanAdalin@gmail.com>
2017-01-28 15:41:52 -0500

    Added base for iOS development

---
+++ mchacks17/mchacks17/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mchacks17
+//
+//  Created by Jonathan Adalin on 2017-01-28.
+//  Copyright © 2017 Jonathan Adalin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
